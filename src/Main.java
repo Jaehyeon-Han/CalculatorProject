656f8e09cb478f26db4f6bc955e460341dc52e7a
Jaehyeon-Han <jaehyeonhan99@gmail.com>
2025-04-16 20:43:42 +0900

    feat: add basic input and evaluation logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int number1 = sc.nextInt();
+        System.out.println("Enter number: ");
+        int number2 = sc.nextInt();
+        System.out.println("Enter operand: ");
+        char operand = sc.nextLine().charAt(0);
+
+        int result = 0;
+        try {
+            switch(operand) {
+                case '+': result = number1 + number2; break;
+                case '-': result = number1 - number2; break;
+                case '/': result = number1 / number2; break;
+                case '*': result = number1 * number2; break;
+                default: throw new Exception("Unknown operand");
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 }
