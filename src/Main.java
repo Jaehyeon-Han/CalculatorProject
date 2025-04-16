afb30e39bbe397bccae75e0cab3504616166f5d5
Jaehyeon Han <jaehyeonhan99@gmail.com>
2025-04-16 21:03:42 +0900

    feat: put input and evaluation logic in a loop

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        int number1 = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter number: ");
-        int number2 = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter operand: ");
-        char operand = sc.nextLine().charAt(0);
+        boolean repeat = false;
 
-        int result = 0;
-        try {
-            switch(operand) {
-                case '+': result = number1 + number2; break;
-                case '-': result = number1 - number2; break;
-                case '/': result = number1 / number2; break;
-                case '*': result = number1 * number2; break;
-                default: throw new Exception("Unknown operand");
+        do {
+            System.out.println("Enter number: ");
+            int number1 = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter number: ");
+            int number2 = Integer.parseInt(sc.nextLine());
+            System.out.println("Enter operand: ");
+            char operand = sc.nextLine().charAt(0);
+
+            int result = 0;
+            try {
+                switch(operand) {
+                    case '+': result = number1 + number2; break;
+                    case '-': result = number1 - number2; break;
+                    case '/': result = number1 / number2; break;
+                    case '*': result = number1 * number2; break;
+                    default: throw new Exception("Unknown operand");
+                }
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
             }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
 
-        System.out.println(result);
-
+            System.out.println("Result: " + result);
+            System.out.println("Enter \"exit\" to stop");
+            repeat = !sc.nextLine().equals("exit");
+        } while(repeat);
     }
 }
